63e3c235a45e5d04e0e9acf4b2290a67a566fd9a
Patrick McNamee <pjmcnamee@comcast.net>
2019-03-19 14:29:43 -0600

    'done'

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,13 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import './App.css';
+import "./App.css";
 
-import Header from './Header/Header';
-import Compose from './Compose/Compose';
+import Header from "./Header/Header";
+import Compose from "./Compose/Compose";
+import Post from "./Post/Post";
+import axios from "axios";
+
+const baseUrl = "https://practiceapi.devmountain.com/api/posts";
 
 class App extends Component {
   constructor() {
@@ -13,25 +17,68 @@ class App extends Component {
       posts: []
     };
 
-    this.updatePost = this.updatePost.bind( this );
-    this.deletePost = this.deletePost.bind( this );
-    this.createPost = this.createPost.bind( this );
+    this.updatePost = this.updatePost.bind(this);
+    this.deletePost = this.deletePost.bind(this);
+    this.createPost = this.createPost.bind(this);
+    this.filterPost = this.filterPost.bind(this);
   }
-  
+
   componentDidMount() {
-
+    axios
+      .get(baseUrl)
+      .then(results => {
+        this.setState({
+          posts: results.data
+        });
+      })
+      .catch(error => ("some error: ", error));
   }
 
-  updatePost() {
-  
+  updatePost(id, text) {
+    axios
+      .put(`${baseUrl}/?id=${id}`, { text })
+      .then(results => {
+        this.setState({
+          posts: results.data
+        });
+      })
+      .catch(error => console.log("some error ", error));
   }
 
-  deletePost() {
-
+  deletePost(id) {
+    axios
+      .delete(`${baseUrl}/?id=${id}`)
+      .then(results => {
+        this.setState({
+          posts: results.data
+        });
+      })
+      .catch(error => console.log("had a error", error));
   }
 
-  createPost() {
+  createPost(text) {
+    axios
+      .post(baseUrl, { text })
+      .then(results =>
+        this.setState({
+          posts: results.data
+        })
+      )
+      .catch(error => console.log("Big ol error ", error));
+  }
 
+  filterPost(text) {
+    console.log("this is the text before conversion", text);
+    let encodedString = encodeURI(text);
+    console.log("this is the encoded string", encodedString);
+    axios
+      .get(`${baseUrl}/filter?text=${encodedString}`)
+      .then(results =>
+        this.setState({
+          posts: results.data
+        })
+      )
+      .catch(error => ("Filter error ", error));
   }
 
   render() {
@@ -39,12 +86,21 @@ class App extends Component {
 
     return (
       <div className="App__parent">
-        <Header />
+        <Header filterPostFn={this.filterPost} />
 
         <section className="App__content">
+          <Compose createPostFn={this.createPost} />
 
-          <Compose />
-          
+          {posts.map(post => (
+            <Post
+              key={post.id}
+              text={post.text}
+              date={post.date}
+              id={post.id}
+              updatePostFn={this.updatePost}
+              deletePostFn={this.deletePost}
+            />
+          ))}
         </section>
       </div>
     );
--- src/components/Header/Search/Search.js
+++ src/components/Header/Search/Search.js
@@ -1,8 +1,7 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import './Search.css';
-
-import SearchIcon from 'react-icons/lib/md/search';
+import "./Search.css";
+import SearchIcon from "react-icons/lib/md/search";
 
 //////////////////////////////////////////////////////// THIS COMPONENT IS BEING RENDERED IN THE *HEADER* COMPONENT
 
@@ -10,14 +9,15 @@ export default class Search extends Component {
   render() {
     return (
       <section className="Search__parent">
-
         <div className="Search__content">
-          <input placeholder="Search Your Feed" />
+          <input
+            onChange={event => this.props.filterPostFn(event.target.value)}
+            placeholder="Search Your Feed"
+          />
 
           <SearchIcon id="Search__icon" />
         </div>
-        
       </section>
-    )
+    );
   }
 }
